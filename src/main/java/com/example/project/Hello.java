59fb20764f1e9cbb1ab53e6afc40852360834ca9
loganmarshall43 <135401419+loganmarshall43@users.noreply.github.com>
2023-06-02 21:23:07 -0600

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -1,10 +1,7 @@
 package com.example.project;
 
 public class Hello {
-
-	public static void main(final String[] args) {
-		System.out.println("Hello World!/n");
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
-
